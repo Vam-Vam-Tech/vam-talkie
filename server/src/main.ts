197e3688e006d06687d2ee27398df239f8c3b20b
htetarkarhlaing <htet.arkar.hlaing.official.mm@gmail.com>
2023-08-30 11:26:06 +0630

    changed: default PORT to env

--- server/src/main.ts
+++ server/src/main.ts
@@ -18,6 +18,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('documentation', app, document);
-  await app.listen(8081);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
